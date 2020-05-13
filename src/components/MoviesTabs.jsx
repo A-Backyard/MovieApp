7f6dc3f067216e3e9fce469047519f3b6d21e953
A-Backyard <anton.backyard@gmail.com>
2020-05-13 14:16:45 +0300

    NavBar

---
+++ src/components/MoviesTabs.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const MoviesTabs = (props) => {
+    const {sort_by, updateSortBy} = props;
+    const handleClick = (value) => {
+        return () => {
+            updateSortBy(value);
+        }
+    }
+    const getClassActive = (value) => {
+        return `nav-link ${sort_by === value ? "active" : ""}`
+    }
+
+    return (
+        <ul className="tabs nav nav-pills">
+            <li className="nav-item">
+                <div className={getClassActive("popularity.desc")}
+                     onClick={handleClick("popularity.desc")}>
+                    Popularity desc
+                </div>
+            </li>
+            <li className="nav-item">
+                <div
+                    className={getClassActive("revenue.desc")}
+                    onClick={handleClick("revenue.desc")}>
+                    Revenue desc
+                </div>
+            </li>
+            <li className="nav-item">
+                <div
+                    className={getClassActive("vote_average.desc")}
+                    onClick={handleClick("vote_average.desc")}>
+                    Vote average desc
+                </div>
+            </li>
+        </ul>
+    )
+}
+
+export default MoviesTabs
